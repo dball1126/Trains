879dfe38404bd254b3fc7e8b646e85f11d0187df
daniel <g.thirteen333@gmail.com>
2019-10-25 20:01:05 -0400

    first commit

---
+++ com/Trains.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+
+public class Trains {
+
+    public static void main(String[] args){
+        System.out.println("hello");
+    }
+}
