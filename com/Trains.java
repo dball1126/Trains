943f0369df686e33334ef8cdd91db3e4b040b638
daniel <g.thirteen333@gmail.com>
2019-10-25 20:43:03 -0400

    nodes are now in an array

--- com/Trains.java
+++ com/Trains.java
@@ -10,11 +10,17 @@ public class Trains {
         File file = new File("/Users/danielball/Desktop/Trains/data.txt");
         BufferedReader br = new BufferedReader(new FileReader(file));
         String st = "";
-        String[] arr = new String[1]; 
+        String[] graph = new String[1]; 
+        while ((st = br.readLine()) != null) graph[0] = st; // Read first line
+        if (graph[0] == null) System.out.println("No input data"); //If first line is null print out not input
         
-        while ((st = br.readLine()) != null) arr[0] = st; // Read first line
-        if (arr[0] == null) System.out.println("No input data"); //If first line is null print out not input
-        
-        System.out.println(arr[0]);
+        String[] nodes = new String[graph[0].length()/2]; // Array of nodes with minimal memory allocation
+        nodes = graph[0].split(","); // All nodes in an array
+
+        for (int i = 0; i < nodes.length; i++) {
+            
+            System.out.println(nodes[i]);
+        }
+
     }
 }
