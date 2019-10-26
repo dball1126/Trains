72e742600a57c9b44305d367c6a69fd93a12ff24
daniel <g.thirteen333@gmail.com>
2019-10-25 20:35:40 -0400

    error message and reader set to attribute

--- com/Trains.java
+++ com/Trains.java
@@ -9,8 +9,12 @@ public class Trains {
         // Read data file
         File file = new File("/Users/danielball/Desktop/Trains/data.txt");
         BufferedReader br = new BufferedReader(new FileReader(file));
+        String st = "";
+        String[] arr = new String[1]; 
         
-        System.out.println(br);
-
+        while ((st = br.readLine()) != null) arr[0] = st; // Read first line
+        if (arr[0] == null) System.out.println("No input data"); //If first line is null print out not input
+        
+        System.out.println(arr[0]);
     }
 }
